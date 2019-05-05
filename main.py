46d7809fdc8179f82b135c92859aabcc886063a9
Your Name <coleyoungmusic@gmail.com>
2019-05-04 21:04:40 -0500

    added imports

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+# TODO: provide unlimited number of entries for bills paid
+# TODO: calculate the finances and provide visual rep. using matplotlib
+# TODO: give suggestion as to how the user can save their money
+# TODO: try to implement different ttk themes
+
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
 from appJar import gui
 
 
-app = gui("Budget App")
 
-app.addLabel("Test")
-# test
+def plotfinances(btn):
+
+
+app = gui("Budget App", "378x265")
+
+
+
+app.addEntry("Payments")
+app.setEntryDefault("Payments", "Enter payment amounts here")
+
+
 
 app.go()
