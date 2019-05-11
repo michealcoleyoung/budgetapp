06fcd2c5451c94cc9813ced9ab7f1ce6c11a9ebb
Your Name <coleyoungmusic@gmail.com>
2019-05-10 23:38:05 -0500

    added simple pyplot to handle values

--- main.py
+++ main.py
@@ -7,43 +7,20 @@ import matplotlib.pyplot as plt
 from appJar import gui
 
 
-def plotfinances(btn):
-    plt.plot([int(app.getEntry("e1")), int(app.getEntry("e2")), int(app.getEntry("e3"))])
-    plt.ylabel('some numbers')
+def plotvalues(btn):
+    values = app.getEntry("e1")
+    new_list = list(map(int, values.split(",")))
+    plt.plot(new_list)
+    plt.ylabel('Payment Made')
     plt.show()
-
-
-
 
 
 app = gui("Budget App", "378x265")
 app.setBg("grey")
 
-
-app.startTabbedFrame("TabbedFrame")  # tab frame start
-
-app.startTab("Expenses")
 app.addEntry("e1")
-app.addEntry("e2")
-app.addEntry("e3")
-app.addEntry("e4")
-app.addEntry("e5")
-app.addEntry("e6")
-app.addEntry("e7")
-app.addEntry("e8")
-app.addEntry("e9")
-app.addEntry("e10")
-app.addButton("PLOT", plotfinances)
+app.addButton("Values", plotvalues)
 
-
-
-app.stopTab()
-
-app.startTab("Graph")
-# This will include the graph with all expenses
-app.stopTab()
-
-app.stopTabbedFrame()  # tab frame end
 
 
 app.go()
