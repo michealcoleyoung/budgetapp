0b6666fc12baa7341275546d90b8d83506b6a1fe
Your Name <coleyoungmusic@gmail.com>
2019-05-13 18:23:57 -0500

    corrected x and y plot descriptions

--- main.py
+++ main.py
@@ -9,24 +9,40 @@ plt.style.use('ggplot')
 
 
 def plotvalues(btn):
-    plt.xlabel("Payment Made")
-    plt.ylabel('Previous Months')
+    plt.xlabel("Months 1 - 4")
+    plt.ylabel('Amounts Payed')
     plt.title("Billing Amounts Payed")
     x = ["month 1", "month 2", "month 3", "month 4"]
     values = app.getEntry("e1")
-    new_list = list(map(int, values.split(",")))
+    new_list = list(map(float, values.split(",")))
     x_pos = [i for i, _ in enumerate(x)]
     plt.bar(x_pos, new_list, color='green')
     plt.xticks(x_pos, x)
+    app.setLabel("s1", new_list[3] - new_list[2])
+
     plt.show()
+
+    # sets amount saved values
+
+
+def clear(btn):
+    app.clearAllEntries()
+    app.clearLabel("s1")
 
 
 app = gui("Budget App", "378x265")
 app.setBg("grey")
 
 app.addEntry("e1")
-app.addButton("Values", plotvalues)
-
+app.addLabel("Savings Month 1")
+app.addLabel("s1", "")
+app.addLabel("Savings Month 2")
+app.addLabel("s2", "")
+app.addLabel("Savings Month 3")
+app.addLabel("s3", "")
+app.addLabel("Savings Month 4")
+app.addLabel("s4", "")
+app.addButtons(["Values", "Clear"], [plotvalues, clear])
 
 
 app.go()
